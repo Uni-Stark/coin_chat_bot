7e2e1a05f9c17fe4da5ee846ed76cc23c4955fc1
Uni-Stark <kgo0748@gmail.com>
2018-12-20 17:25:38 +0900

    다나와를 못불러온다 속상하다

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import json
 import os
 import re
@@ -9,41 +8,60 @@ from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
-slack_token = 'xoxb-502213453520-507487078738-UNV98eoaolTb9z7HW73UlMiD'
+slack_token = 'xoxb-502213453520-507487078738-pspACr4G8UxxoMoQ8KOc1jco'
 slack_client_id = '502213453520.507317959924'
 slack_client_secret = '7482236f1d9a0063a4eb4019f2131847'
 slack_verification = 'ixJm1RO9AtuideG76KuEbHh3'
 sc = SlackClient(slack_token)
 
-# 크롤링 함수 구현하기
-def _crawl_naver_keywords(text):
 
-    url = re.search(r'(https?://\S+)', text.split('|')[0]).group(0)
-    req = urllib.request.Request(url)
-
-    sourcecode = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(sourcecode, "html.parser")
-
-    keywords = []
-    keywords.append("실시간 코인 시세 Top 100\n")
-    for i, coin in enumerate(soup.find("tbody").find_all("a", class_="currency-name-container link-secondary")):
-        if i<50:
-            keywords.append(str(i+1) + " 위:" + coin.get_text().replace('\n', '') + " || 가격: ")
-    for i, price in enumerate(soup.find("tbody").find_all("a", class_="price")):
-        if i<50:
-            keywords[i+1] += price.get_text().replace('\n', '')+'\n'
-
-    return u'\n'.join(keywords)
-# 이벤트 핸들하는 함수
-
-def _get_coin_currency():
+def _crawl_coin_currency(text):
     url = "https://www.coinmarketcap.com/ko"
 
 # URL 주소에 있는 HTML 코드를 soup에 저장합니다.
     soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
 
-    keyword=[]
-    keyword.append("실시간 코인 시세 Top 50")
+    cur = []
+    cur.append("실시간 코인 시세 Top 10\n")
+    for i, coin in enumerate(soup.find("tbody").find_all("a", class_="currency-name-container link-secondary")):
+        if i < 10:
+            cur.append(str(i+1) + " 위:" + coin.get_text().replace('\n', '') + " || 가격: ")
+    for i, price in enumerate(soup.find("tbody").find_all("a", class_="price")):
+        if i < 10:
+            cur[i+1] += price.get_text().replace('\n', '')+ '\n'
+
+    return u'\n'.join(cur)
+
+def _get_coin_currency(text):
+    url = "https://www.coinmarketcap.com/ko"
+    soup = BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser")
+    name = []
+
+    for i, coin in enumerate(soup.find("tbody").find_all("a", class_="currency-name-container link-secondary")):
+        if coin is text:
+            name.append(coin.get_text().replace('\n', ''))
+
+
+                    # else:
+                        # price[i+1] += j.get_text().replace('\n', '')
+            # else:
+            #     name.coin.get_text().replace('\n', '')
+            #     for j in coin.find("a",class_="price"):
+            #         if price is None:
+            #             price.append(j.get_text().replace('\n',''))
+            #         else:
+            #             price[i+1] += j.get_text().replace('\n', '')
+    for i , price in enumerate(soup.find("tbody").find_all("a", class_="price")):
+        if i < 10:
+            price.append(coin.get_text().replace('\n', ''))
+    result = dict(zip([coin_name for coin_name in name], [coin_price for coin_price in price]))
+
+
+
+    final_result = result[text]
+    return final_result
+
+
 def _event_handler(event_type, slack_event):
     print(slack_event["event"])
 
@@ -51,20 +69,25 @@ def _event_handler(event_type, slack_event):
         channel = slack_event["event"]["channel"]
         text = slack_event["event"]["text"]
 
-        keywords = _crawl_naver_keywords(text)
-        sc.api_call(
-            "chat.postMessage",
-            channel=channel,
-            text=keywords
-        )
-
+        if '코인' in text:
+            keywords = _crawl_coin_currency(text)
+            sc.api_call(
+                "chat.postMessage",
+                channel=channel,
+                text=keywords
+            )
+        else :
+            keywords = _get_coin_currency(text)
+            sc.api_call(
+                "chat.postMessage",
+                channel=channel,
+                text=keywords
+            )
         return make_response("App mention message has been sent", 200,)
 
-    # ============= Event Type Not Found! ============= #
-    # If the event_type does not have a handler
     message = "You have not added an event handler for the %s" % event_type
-    # Return a helpful error message
-    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+
+    return make_response(message, 200, {"X-Slack-No_Retry": 1})
 
 @app.route("/listening", methods=["GET", "POST"])
 def hears():
@@ -83,8 +106,6 @@ def hears():
         event_type = slack_event["event"]["type"]
         return _event_handler(event_type, slack_event)
 
-    # If our bot hears things that are not events we've subscribed to,
-    # send a quirky but helpful error response
     return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
                          you're looking for.", 404, {"X-Slack-No-Retry": 1})
 
